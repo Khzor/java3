818eb8d5c048e7f668f7c9957889358353eadc89
Goncharenko Filipp <phil.can@yandex.ru>
2021-07-19 17:56:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket_price = 4000;
+        int miles = ticket_price / 20;
+
+        System.out.println(miles);
+    }
+}
